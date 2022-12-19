f90c549b76307c064bf7c3b609d78f5b37e3e6a3
Rojsun <rojsun25@gmail.com>
2022-12-19 13:20:47 +0000

    added comments

--- filestorage.py
+++ filestorage.py
@@ -1,4 +1,4 @@
 from azure_ETL_data_pipelines.blob_storage import uploadToBlobStorage
 
-# calling a function to perform upload
+# calling a function to perform upload file_path,filename
 uploadToBlobStorage(' ', ' ')
